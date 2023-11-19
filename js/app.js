32deb55b01317bcfcebb006b6a4ca161bf3348dd
Andrey Frolov <senior.slender2015@gmail.com>
2023-11-19 22:46:01 +0200

    JS. Parallax on scrolling

--- js/app.js
+++ js/app.js
@@ -50,6 +50,27 @@ window.onload = function () {
             // Receipt of interest 
             coordXprocent = coordX / parallaxWidth * 100;
             coordYprocent = coordY / parallaxHeight * 100;
-        })
+        });
+
+        // Parallax when scrolling 
+        let tresholdSets = [];
+        for (let i = 0; i < 1.0; i += 0.005) {
+            tresholdSets.push(i);
+        }
+        const callback = function (entries, observer) {
+            const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
+            setParallaxItemsStyle(scrollTopProcent);
+        };
+        const observer = new IntersectionObserver(callback, {
+            threshold: tresholdSets
+        });
+
+        observer.observe(document.querySelector(".content"));
+
+        function setParallaxItemsStyle(scrollTopProcent) {
+            content.style.cssText = `transform: translate(0%, -${scrollTopProcent / 9}%);`;
+            mountains.parentElement.style.cssText = `transform: translate(0%, -${scrollTopProcent / 6}%);`;
+            human.parentElement.style.cssText = `transform: translate(0%, -${scrollTopProcent / 3}%);`;
+        }
     }
 }
